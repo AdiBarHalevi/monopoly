803b000c3625175d7cda573f49546981266bb77c
AdiBarHalevi <adibar141@gmail.com>
2021-04-29 14:16:49 +0300

    made a small server that connects with the front end first deployment effort

--- server/index.js
+++ server/index.js
@@ -26,9 +26,26 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`application start at ${process.env.PORT || 5000}`)
-})
+
+
+// app.listen(process.env.PORT || 5000, () => {
+//     console.log(`application start at ${process.env.PORT || 5000}`)
+// })
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+app.listen(process.env.PORT || port , () =>{
+    console.log(`Server started on port ${port}`)
+});
+
 
 
 
