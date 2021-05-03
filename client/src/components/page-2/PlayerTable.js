74e7f95d4ce0e14752168791b5e6228f51c36d23
AdiBarHalevi <adibar141@gmail.com>
2021-05-03 16:02:13 +0300

    appended turn managment system

--- client/src/components/page-2/PlayerTable.js
+++ client/src/components/page-2/PlayerTable.js
@@ -1,34 +1,50 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import {GamePlayDataState} from "../../atoms"
+import styled from "styled-components";
+
+
 
 const PlayerTable = () => {
-  return (
+  const [playersDataState, setPlayersDataState]= useRecoilState(GamePlayDataState)
+
+  if(playersDataState) return (
     <>
-      <table>
+      <GameStatus>
         <thead>
           <tr>
-            <th>Player's Turn</th>
-            <th>Player's Name</th>
-            <th>Balance</th>
+            <Td>player</Td>
+            <Td> Name </Td>
+            <Td> Balance </Td>
+            <Td> Location </Td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Adi</td>
-
-            <td>100000</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Lior</td>
-            <td>200000</td>
-          </tr>
+          {
+          playersDataState.map((player,i)=>{
+            return(
+            <tr key={i}>
+              <Td>{player.turnNum}</Td>
+              <Td>{player.name}</Td>
+              <Td>{player.balance}</Td>
+              <Td>{player.playerLocation}</Td>
+            </tr>)
+          })}
         </tbody>
-      </table>
+      </GameStatus>
     </>
   );
 };
 
 export default PlayerTable;
 
+const GameStatus = styled.table`
+  text-align:center;
+  font-size:12px;
+`;
 
+const Td = styled.td`
+  background:pink;
+  border:1px solid;
+  width:6rem
+`
--- client/src/components/page-2/game-managment/GameManager.js
+++ client/src/components/page-2/game-managment/GameManager.js
@@ -1,41 +1,101 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import {GamePlayDataState} from "../../../atoms"
 import styled from "styled-components";
 import PlayerManager from "./PlayerManager"
+import {getRandomInt} from "../../../UtilityFunctions"
 
 
 const GameManager = () => {
 
-  const [dice,setDice] = useState([0,0])
+  const [activePlayerState,setActivePlayerState] = useState({})
+  const [diceState,setdiceState] = useState([0,0,0])
+  const [playersDataState, setPlayersDataState]= useRecoilState(GamePlayDataState)
 
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+  const saveToGlobalUserData=()=>{
+    const update = []
+    playersDataState.map((player)=>{
+      if(player.name === (activePlayerState.name)) {
+        const newPlayerStatus={
+          ...player
+        }
+        newPlayerStatus[`playerLocation`]= activePlayerState.playerLocation
+        update.push(newPlayerStatus)
+      } else update.push(player)
+    })
+    setPlayersDataState(update)
+
   }
-  const RollDice =()=> setDice([getRandomInt(1,7),getRandomInt(1,7)])
+
+
+  // finishes the players Turn and sets the Active user to the next player
+  const endTurn =()=>{
+    saveToGlobalUserData()
+    let whosturn =  diceState[2]
+    whosturn++
+    if(whosturn < playersDataState.length) {
+      const activeUser = playersDataState[whosturn]
+      setdiceState([diceState[0],diceState[1], whosturn])
+      setActivePlayerState(activeUser)
+    }
+    else{
+      whosturn=0
+      const activeUser = playersDataState[0]
+      setdiceState([diceState[0],diceState[1], whosturn])
+      setActivePlayerState(activeUser)
+    }
+  }
+
+  // genereate Dice roll (keeps the players turn)
+  const RollDice =()=>{ 
+
+    setdiceState([getRandomInt(1,7),getRandomInt(1,7), diceState[2]])
+
+    // set changes on the Active User State
+    const update = {...activePlayerState}
+    console.log(diceState[0]+diceState[1])
+    update[`playerLocation`]+=(diceState[0]+diceState[1])
+    setActivePlayerState(update)
+
+  }
+
+  // useEffect(()=>{
+  //   setActivePlayerState(playersDataState[0])
+  //   setdiceState([0,0,0])
+  //   console.log(activePlayerState)
+
+  // },[])
+
+  useEffect(()=>{
+    setActivePlayerState(playersDataState[diceState[2]])
+    
+  },[])
   
   
-  
+  if(activePlayerState){
   return (
     <TurnTable>
       <PlayerManager/>
-
       <tr>
         <th>Player's Turn</th>
-        <td>Adi</td>
+        <td>{activePlayerState.name}</td>
       </tr>
       <tr>
         <th>Current Location</th>
-        <td>0</td>
+        <td>{activePlayerState.playerLocation}</td>
       </tr>
       <tr>
+        <td><button onClick={endTurn}>End turn</button></td>
         <td><button onClick={RollDice}>Roll Dice</button></td>
-        <td><span>dice result</span> <span>{dice[0]+dice[1]}</span></td>
-        
+        {/* <td><span>dice result</span> <span>{activePlayerState.playerLocation}</span></td> */}
       </tr>
   </TurnTable>
   );
+} else return (
+    <TurnTable>
+      <PlayerManager/>
+    </TurnTable>
+    )
 };
 
 export default GameManager;
